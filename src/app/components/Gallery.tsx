a74c07375e77fa74c9a022ddfe4739ce60026656
Nemidas <nehalchaudhari5302@gmail.com>
2024-06-08 11:01:36 +0530

    Modify Gallery component

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image"
 export default function Gallery(){
     return(
-        <div className="flex-col justify-center mt-14">
+        <div className="flex-col justify-center mt-14 mb-20">
             <h2 className="flex justify-center font-bold text-4xl p-6 ">Gallery</h2>
-            <div className="flex justify-center gap-4 mt-4 mb-10">
-                <Image src="/images/Ellipse 1.png" alt="" height={240} width={260} quality={100} className="w-[265px] h-[227px]"/>
-                <Image src="/images/Ellipse 2.png" alt="" height={240} width={260} quality={100} className="w-[265px] h-[227px]"/>
-                <Image src="/images/Ellipse 1.png" alt="" height={240} width={260} quality={100} className="w-[265px] h-[227px]"/>
-                <Image src="/images/Ellipse 2.png" alt="" height={240} width={260} quality={100} className="w-[265px] h-[227px]"/>
+            <div className="flex justify-center gap-4">
+                <Image src="/gallery/gallery_4.jpeg" alt="" height={240} width={660} quality={100} style={{
+                    objectFit: 'cover',
+                    objectPosition: '60% 20%',
+                  }}  className="w-[265px] h-[227px]"/>
+                <Image src="/gallery/gallery_3.jpeg" alt="" height={240} width={660} quality={100} style ={{objectFit:'cover', objectPosition:"20% 25%"}} className="w-[265px] h-[227px] object-cover"/>
+                <Image src="/gallery/gallery_2.jpeg" alt="" height={260} width={660} quality={100} style ={{objectFit:'cover', objectPosition:"20% 25%"}} className="w-[265px] h-[227px] object-cover"/>
+                <Image src="/gallery/gallery_1.jpeg" alt="" height={240} width={660} quality={100} style ={{objectFit:'cover', objectPosition:"20% 25%"}} className="w-[265px] h-[227px] object-cover"/>
             </div>
         </div>
     )
